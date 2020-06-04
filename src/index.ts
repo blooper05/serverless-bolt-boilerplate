78b781d29c8b8d73c49a01bdd3df1109d25f6712
blooper05 <legend.of.blooper@gmail.com>
2020-06-04 11:57:38 +0900

    :recycle: Truncate setting values to variables

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { App, ExpressReceiver } from '@slack/bolt';
 
-const expressReceiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  processBeforeResponse: false,
-});
+const signingSecret = process.env.SLACK_SIGNING_SECRET;
+const token = process.env.SLACK_BOT_TOKEN;
+const processBeforeResponse = false;
 
-const app = new App({
-  receiver: expressReceiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  processBeforeResponse: false,
-});
+const receiver = new ExpressReceiver({ signingSecret, processBeforeResponse });
+const app = new App({ token, receiver });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,19 +1,14 @@
 import { App, ExpressReceiver } from '@slack/bolt';
 import { createServer, proxy } from 'aws-serverless-express';
 
-const expressReceiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  processBeforeResponse: true,
-});
+const signingSecret = process.env.SLACK_SIGNING_SECRET;
+const token = process.env.SLACK_BOT_TOKEN;
+const processBeforeResponse = true;
 
-const app = new App({
-  receiver: expressReceiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  processBeforeResponse: true,
-});
-
-const server = createServer(expressReceiver.app);
+const receiver = new ExpressReceiver({ signingSecret, processBeforeResponse });
+const app = new App({ token, receiver });
 
 module.exports.handler = async (event, context) => {
+  const server = createServer(receiver.app);
   return proxy(server, event, context, 'PROMISE').promise;
 };
