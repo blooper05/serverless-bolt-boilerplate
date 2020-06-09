edc04695bdc3ad6c86d54153674c37c4aba40ebc
blooper05 <legend.of.blooper@gmail.com>
2020-06-09 12:35:45 +0900

    :sparkles: Add an event listener for app_mention

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,18 @@
 import { App } from '@slack/bolt';
 
 export const initListener = async (app: App): Promise<void> => {
+  app.event('app_mention', async ({ event, say }) => {
+    await say({
+      text: '',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Hi! <@${event.user}> :wave:`,
+          },
+        },
+      ],
+    });
+  });
 }
