d30646be919c7f4c0b20c45589d4e2986f8cdf2c
blooper05 <legend.of.blooper@gmail.com>
2020-06-09 23:11:43 +0900

    :recycle: Improve typings

--- src/app.view.tsx
+++ src/app.view.tsx
@@ -1,7 +1,8 @@
 /** @jsx JSXSlack.h */
 import { JSXSlack, Blocks, Section } from '@speee-js/jsx-slack';
+import { Block } from '@slack/types';
 
-export const helloWorld = ({ name }: { name: string }): any => {
+export const helloWorld = ({ name }: { name: string }): Block[] => {
   return JSXSlack(
     <Blocks>
       <Section>
